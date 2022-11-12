0d4760b789481500dbcb8d1c6695447cde9603fc
E-Guevs <101308494+E-Guevs@users.noreply.github.com>
2022-11-13 00:09:02 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const checkbox = document.getElementById("hamburger-icon"),
-  contents = document.querySelectorAll("#navbar li");
-for (let content of contents)
-  content.addEventListener("pointerup", () => (checkbox.checked = false));
